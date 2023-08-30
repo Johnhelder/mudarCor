05365b7eaf2ab1de2617b850c83b720b27b34700
John Helder <johnhelderjp@gmail.com>
2023-08-29 21:37:07 -0300

    Criando função para gerar números aleatórios

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,4 +4,10 @@ const btn = document.getElementById('btn');
 
 btn.addEventListener('click', function(){
   document.body.style.background = cores[3];
+  const randomColor = getRandomColor();
+  console.log(randomColor);
 })
+
+function getRandomColor(){
+  return Math.floor(Math.random() * cores.length)
+}
